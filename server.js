a2948e580e8cbee750d1f65820f6da49b2706539
Bert Keller <bertwinkeller@gmail.com>
2020-09-14 13:31:12 -0700

    connected to the DB

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+const connectDB = require('./config/db.js')
+
+//Connect to DB
+connectDB()
+
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
 
 app.get('/', (req,res) => {
     res.send('API is running')
-})
+}) 
