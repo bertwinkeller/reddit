542feff968465ed7feec4cfa2e802cd0c27a3815
Bert Keller <bertwinkeller@gmail.com>
2020-09-14 20:25:59 -0700

    create user model

--- server.js
+++ server.js
@@ -14,3 +14,9 @@ app.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
 app.get('/', (req,res) => {
     res.send('API is running')
 }) 
+
+
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/auth', require('./routes/api/auth'))
