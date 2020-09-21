b38f77ff6709fa5be02758f64885dfbbd6d88162
Bert Keller <bertwinkeller@gmail.com>
2020-09-21 12:27:04 -0700

    building user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult} = require('express-validator/check')
 
+const User = require('../../models/User')
 
-router.post('/', (req, res) => {
+router.post('/', [
+ check('name', 'Name is required')
+ .not()
+ .isEmpty() ,
+ check('email', 'Please include a valid email').isEmail(),
+ check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+    
+],
+ async (req, res) => {
+    console.log(req.body)
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    try{
+    //Check if the user exists
+    const{name, email, password} = req.body
+    //Get gravatar
+
+    //Enctrypt Pword
+
+    //Return JsonWebToken 
     res.send('Users route')
+
+    }catch(err){
+      console.error(err.message)
+      res.status(500).send('server error')
+    }
 })
 
 module.exports = router
