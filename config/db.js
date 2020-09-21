22ae2b772564420d6a2cdbe9cedd69f9d8a461ac
Bert Keller <bertwinkeller@gmail.com>
2020-09-21 13:15:26 -0700

    jwt set up

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db = config.get('mongoURI')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+        await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
         console.log('Youre connected to the database')
 
     } catch (err) {
